10565e81b472376799d82c6c3bbe78e138b4ade1
SACHIN NANDHA SABARISH <sachin@SACHINs-Laptop.local>
2025-04-08 03:18:17 +0530

    Stable 2

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/theme-provider";
+import { NotistackProvider } from "@/components/providers/snackbar-provider";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -18,8 +19,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
         disableTransitionOnChange
       >
         <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
+          <NotistackProvider>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </NotistackProvider>
         </QueryClientProvider>
       </ThemeProvider>
     </SessionProvider>
